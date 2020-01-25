f89c6d7ee874ac6f6a63b667d7e5202d6413d153
Omid Mirrajei <omiddeadlive@gmail.com>
2020-01-25 23:23:44 +0330

    Initial commit

---
+++ app/src/main/java/com/applike/geoquiz/QuizViewModel.kt
@@ -0,0 +1,43 @@
+package com.applike.geoquiz
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+
+private const val TAG = "QuizViewModel"
+
+class QuizViewModel : ViewModel() {
+
+    init {
+        Log.d(TAG, "ViewModel instance")
+    }
+
+    // The onCleared() function is called just before a ViewModel is destroyed.
+    // This is a useful place to perform any cleanup, such as un-observing a data source.
+    override fun onCleared() {
+        super.onCleared()
+        Log.d(TAG, "ViewModel instance about to be destroyed")
+    }
+
+    var currentIndex = 0
+    var isCheater = false
+
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true)
+    )
+
+    val currentQuestionAnswer: Boolean
+        get() = questionBank[currentIndex].answer
+
+    val currentQuestionText: Int
+        get() = questionBank[currentIndex].textResId
+
+    fun moveToNext() {
+        currentIndex = (currentIndex + 1) % questionBank.size
+    }
+
+}
